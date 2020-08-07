e03c95452a434477e73739af447bc9636a5f7e5f
Romario Alves <Romariogn10@gmail.com>
2020-08-07 15:48:08 -0300

    finalzilacao do terceiro dia e criacao do botao adicionar hora

--- scripts/addField.js
+++ scripts/addField.js
@@ -1,1 +1,20 @@
-document.querySelector("#container").style.backgroundColor = "red";
+// procurar o botao.
+document.querySelector("#add-time").addEventListener('click', cloneField)
+//quando clicar no botao.
+// executar uma acao.
+function cloneField() {
+    //duplicar a acao.
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
+    
+    //pegar os campos
+    const fields = newFieldContainer.querySelectorAll('input')
+    // para cada campo, limpar
+    fields.forEach(function (field){
+        //pega o field atual e limpa
+        field.value = ""
+    })
+    //colocar na pagina.
+    document.querySelector('#schelude-items').appendChild(newFieldContainer)
+}
+
+
