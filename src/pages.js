1d261d3f150715daf5c485ccd961cd0a91b7bf17
Romario Alves <Romariogn10@gmail.com>
2020-08-10 10:39:57 -0300

    Resolvendo bugs de criacao dde perfil no  banco de dados

--- src/pages.js
+++ src/pages.js
@@ -78,9 +78,9 @@ async function saveClasses(req, res){
         const db = await Database
         await createProffy(db, {proffyValue, classValue, classScheduleValues})
        
-       let queryString = "?subject" + req.body.subjects
-       queryString += "&weekday=" + req.body.weekday[0]
-       queryString += "&time=" + req.body.time_from[0]
+        let queryString = `?subject=${req.body.subject}`
+        queryString += `&weekday=${req.body.weekday[0]}`
+        queryString += `&time=${req.body.time_from[0]}`
 
        
         return res.redirect("/study"+ queryString)
